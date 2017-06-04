fd28e316843e22120e2bc5aa6de92ad5c7716747
Tannz0rz <nabbz0rz@gmail.com>
2017-06-04 14:59:17 -0400

    Update quickgriddefinition.cpp

--- Quick/quickgriddefinition.cpp
+++ Quick/quickgriddefinition.cpp
@@ -8,68 +8,156 @@ QuickGridDefinition::QuickGridDefinition() :
 {
 }
 
-void QuickGridDefinition::addRowDefinition(qreal size)
+void QuickGridDefinition::addRowDefinition(qreal weight, qint32 row)
 {
-    _rows << QuickGridCell(size, _rowSize);
+    if(row < 0 || row >= rowCount())
+    {
+        _rows << QuickGridCell(weight, _rowSize);
+    }
+    else
+    {
+        _rows.insert(row, QuickGridCell(weight, _rows[row]._offset));
 
-    _rowSize += size;
+        for(row = row + 1; row < rowCount(); row++)
+        {
+            _rows[row]._offset += weight;
+        }
+    }
+
+    _rowSize += weight;
     _invRowSize = 1.0f / _rowSize;
 }
 
-void QuickGridDefinition::addColumnDefinition(qreal size)
+void QuickGridDefinition::addColumnDefinition(qreal weight, qint32 column)
 {
-    _columns << QuickGridCell(size, _columnSize);
+    if(column < 0 || column >= columnCount())
+    {
+        _columns << QuickGridCell(weight, _columnSize);
+    }
+    else
+    {
+        _columns.insert(column, QuickGridCell(weight, _columns[column]._offset));
 
-    _columnSize += size;
+        for(column = column + 1; column < columnCount(); column++)
+        {
+            _columns[column]._offset += weight;
+        }
+    }
+
+    _columnSize += weight;
     _invColumnSize = 1.0f / _columnSize;
 }
 
-int QuickGridDefinition::rowCount() const
+void QuickGridDefinition::removeRowDefinition(qint32 row)
+{
+    if(rowCount() == 0)
+    {
+        return;
+    }
+
+    qreal
+            weight;
+
+    if(row < 0 || row >= rowCount())
+    {
+        weight = _rows[rowCount() - 1]._weight;
+
+        _rows.removeLast();
+    }
+    else
+    {
+        weight = _rows[row]._weight;
+
+        _rows.removeAt(row);
+
+        for( ; row < rowCount(); row++)
+        {
+            _rows[row]._offset -= weight;
+        }
+    }
+
+    _rowSize -= weight;
+    _invRowSize = 1.0f / _rowSize;
+}
+
+void QuickGridDefinition::removeColumnDefinition(qint32 column)
+{
+    if(columnCount() == 0)
+    {
+        return;
+    }
+
+    qreal
+            weight;
+
+    if(column < 0 || column >= columnCount())
+    {
+        weight = _columns[columnCount() - 1]._weight;
+
+        _columns.removeLast();
+    }
+    else
+    {
+        weight = _columns[column]._weight;
+
+        _columns.removeAt(column);
+
+        for( ; column < columnCount(); column++)
+        {
+            _columns[column]._offset -= weight;
+        }
+    }
+
+    _columnSize -= weight;
+    _invColumnSize = 1.0f / _columnSize;
+}
+
+qint32 QuickGridDefinition::rowCount() const
 {
     return _rows.size();
 }
 
-int QuickGridDefinition::columnCount() const
+qint32 QuickGridDefinition::columnCount() const
 {
     return _columns.size();
 }
 
-qreal QuickGridDefinition::rowOffset(int index)
+qreal QuickGridDefinition::rowOffset(qint32 index)
 {
     return _rows[index]._offset * _invRowSize;
 }
 
-qreal QuickGridDefinition::columnOffset(int index)
+qreal QuickGridDefinition::columnOffset(qint32 index)
 {
     return _columns[index]._offset * _invColumnSize;
 }
 
-qreal QuickGridDefinition::rowWeight(int index)
+qreal QuickGridDefinition::rowWeight(qint32 index)
 {
     return _rows[index]._weight * _invRowSize;
 }
 
-qreal QuickGridDefinition::columnWeight(int index)
+qreal QuickGridDefinition::columnWeight(qint32 index)
 {
     return _columns[index]._weight * _invColumnSize;
 }
 
-QPointF QuickGridDefinition::cellPoint(QRectF rect, int row, int column)
+QPointF QuickGridDefinition::cellPoint(QRectF rect, qint32 row, qint32 column)
 {
     return QPointF(columnOffset(column) * rect.width() + rect.x(), rowOffset(row) * rect.height() + rect.y());
 }
 
-QSizeF QuickGridDefinition::cellSize(QRectF rect, int row, int column, int rowSpan, int columnSpan)
+QSizeF QuickGridDefinition::cellSize(QRectF rect, qint32 row, qint32 column, qint32 rowSpan, qint32 columnSpan)
 {
     QSizeF
         size;
 
-    for(int end = qMin(row + rowSpan, rowCount()); row < end; row++)
+    for(qint32 end = qMin(row + rowSpan, rowCount()); row < end; row++)
     {
         size.rheight() += rowWeight(row) * rect.height();
     }
 
-    for(int end = qMin(column + columnSpan, columnCount()); column < end; column++)
+    for(qint32 end = qMin(column + columnSpan, columnCount()); column < end; column++)
     {
         size.rwidth() += columnWeight(column) * rect.width();
     }
@@ -77,14 +165,14 @@ QSizeF QuickGridDefinition::cellSize(QRectF rect, int row, int column, int rowSp
     return size;
 }
 
-QRectF QuickGridDefinition::cellRect(QRectF rect, int row, int column, int rowSpan, int columnSpan)
+QRectF QuickGridDefinition::cellRect(QRectF rect, qint32 row, qint32 column, qint32 rowSpan, qint32 columnSpan)
 {
     return QRectF(cellPoint(rect, row, column), cellSize(rect, row, column, rowSpan, columnSpan));
 }
 
-void QuickGridDefinition::calculateBounds(int &row, int &column, int &rowSpan, int &columnSpan)
+void QuickGridDefinition::calculateBounds(qint32 &row, qint32 &column, qint32 &rowSpan, qint32 &columnSpan)
 {
-    int
+    qint32
         rowSize = rowCount(),
         columnSize = columnCount();
 
