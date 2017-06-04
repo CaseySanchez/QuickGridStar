2a92ea1275ff05ecb0339609f3c7ce902bd921e3
Tannz0rz <nabbz0rz@gmail.com>
2017-06-04 15:18:54 -0400

    Update quickgridstar.h

--- Quick/quickgridstar.h
+++ Quick/quickgridstar.h
@@ -3,7 +3,6 @@
 
 #include <QQuickItem>
 #include <QQuickWindow>
-#include <QDebug>
 
 #include "quickgriddefinition.h"
 
@@ -48,43 +47,42 @@ class QuickGridStarAttached : public QObject
     Q_OBJECT
 
     Q_PROPERTY(bool ignore MEMBER _ignore)
-    Q_PROPERTY(int row MEMBER _row)
-    Q_PROPERTY(int column MEMBER _column)
-    Q_PROPERTY(int rowSpan MEMBER _rowSpan)
-    Q_PROPERTY(int columnSpan MEMBER _columnSpan)
+    Q_PROPERTY(qint32 row READ getRow WRITE setRow)
+    Q_PROPERTY(qint32 column READ getColumn WRITE setColumn)
+    Q_PROPERTY(qint32 rowSpan READ getRowSpan WRITE setRowSpan)
+    Q_PROPERTY(qint32 columnSpan READ getColumnSpan WRITE setColumnSpan)
 
 public:
     QuickGridStarAttached(QObject *object);
 
+    void setRow(qint32 row);
+    void setColumn(qint32 column);
+    void setRowSpan(qint32 rowSpan);
+    void setColumnSpan(qint32 columnSpan);
+
+    qint32 getRow();
+    qint32 getColumn();
+    qint32 getRowSpan();
+    qint32 getColumnSpan();
+
 protected:
     bool
-        _ignore;
-    int
+        _ignore,
+        _dirty;
+    qint32
         _row,
         _column,
         _rowSpan,
-        _columnSpan;
+        _columnSpan,
+        _rowSpanActual,
+        _columnSpanActual;
 };
 
 class QuickGridStar : public QQuickItem
 {
     Q_OBJECT
 
-    struct QuickGridItem
-    {
-        QQuickItem
-            *_item;
-        QuickGridStarAttached
-            *_attached;
-
-        QuickGridItem(QQuickItem *item, QuickGridStarAttached *attached) :
-            _item(item),
-            _attached(attached)
-        {
-        }
-    };
-
-    QList<QuickGridItem *>
+    QList<QQuickItem *>
         _items;
 
     QuickGridDefinition
@@ -93,6 +91,20 @@ class QuickGridStar : public QQuickItem
 public:
     QuickGridStar(QQuickItem *parent = nullptr);
     ~QuickGridStar();
+
+    Q_INVOKABLE qint32 rowCount();
+    Q_INVOKABLE qint32 columnCount();
+
+    Q_INVOKABLE QVariant itemsAt(qint32 row, qint32 column);
+
+    Q_INVOKABLE void addItem(QObject *object, qint32 row = 0, qint32 column = 0, qint32 rowSpan = 1, qint32 columnSpan = 1);
+    Q_INVOKABLE bool removeItem(QObject *object);
+
+    Q_INVOKABLE void addRowDefinition(qreal weight = 1.0f, qint32 row = -1);
+    Q_INVOKABLE void addColumnDefinition(qreal weight = 1.0f, qint32 column = -1);
+
+    Q_INVOKABLE void removeRowDefinition(qint32 row = -1);
+    Q_INVOKABLE void removeColumnDefinition(qint32 column = -1);
 
     static QuickGridStarAttached *qmlAttachedProperties(QObject *object);
 
